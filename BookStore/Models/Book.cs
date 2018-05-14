4480cf43eba3b214a88efc6f08280b0a696b6e0d
andrew lupton <aflupton@gmail.com>
2018-05-14 10:29:55 -0700

    add find(), getall(), and addcustomertobook() methods to book.cs

--- BookStore/Models/Book.cs
+++ BookStore/Models/Book.cs
@@ -111,15 +111,103 @@ namespace BookStore.Models
         conn.Dispose();
       }
     }
-      //create join table instance
+    //create join table instance
+    public void AddCustomerToBook(Customer newCustomer)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO books_customers(book_id, customer_id) VALUES (@BookId, @CustomerId);";
 
-      //find instance in table 'books'
+      MySqlParameter book_id = new MySqlParameter();
+      book_id.ParameterName = "@BookId";
+      book_id.Value = _id;
+      cmd.Parameters.Add(book_id);
 
-      //update instance in table 'books'
+      MySqlParameter customer_id = new MySqlParameter();
+      customer_id.ParameterName = "@CustomerId";
+      customer_id.Value = newCustomer.GetId();
+      cmd.Parameters.Add(customer_id);
 
-      //delete instance in table 'books'
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if(conn != null)
+      {
+        conn.Dispose();
+      }
+    }
 
-      //delete entire class
+    //getall book instances in class
+    public static List<Book> GetAll()
+    {
+      List<Book> allBooks = new List<Book> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * from books;";
+
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while (rdr.Read())
+      {
+        int id = rdr.GetInt32(0);
+        string bookName = rdr.GetString(1);
+        string author = rdr.GetString(2);
+        int isbn = rdr.GetInt32(3);
+        double price = rdr.GetDouble(4);
+        string image = rdr.GetString(5);
+        Book newBook = new Book(bookName, author, isbn, price, image, id);
+        allBooks.Add(newBook);
+      }
+      conn.Close();
+      if(conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+    //find instance in table 'books'
+    public static Book Find(int id)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM books WHERE id = (@searchId);";
+
+      MySqlParameter searchId = new MySqlParameter();
+      searchId.ParameterName = "@searchId";
+      searchId.Value = id;
+      cmd.Parameters.Add(searchId);
+
+      MySqlDataReader rdr = cmd.ExecuteReader as MySqlDataReader;
+      int id = 0;
+      string name = "";
+      string author = "";
+      int isbn = 0;
+      double price = 0;
+      string image = "";
+
+      while(rdr.Read())
+      {
+        id = rdr.GetInt32(0);
+        name = rdr.GetString(1);
+        author = rdr.GetString(2);
+        isbn = rdr.GetInt32(3);
+        price = rdr.GetDouble(4);
+        image = rdr.GetString(5);
+      }
+
+      Book myBook = new Book (id, name, author, isbn, price, image);
+      conn.Close();
+      if(conn != null)
+      {
+        conn.Dispose();
+      }
+      return myBook;
+    }
+    //update instance in table 'books'
+
+    //delete instance in table 'books'
+
+    //delete entire class
     public void DeleteAll()
     {
       MySqlConnection conn = DB.Connection();

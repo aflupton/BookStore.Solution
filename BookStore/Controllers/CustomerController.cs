f0cc602f283fdaf7cc3d2bd4b75297a22795e66e
andrew lupton <aflupton@gmail.com>
2018-05-17 11:14:25 -0700

    add html changes

--- BookStore/Controllers/CustomerController.cs
+++ BookStore/Controllers/CustomerController.cs
@@ -46,17 +46,13 @@ namespace Bookstore.Controllers
       return RedirectToAction("Index");
     }
 
-    [HttpPost("/customers/{id}/delete")]
-    public ActionResult DeleteCustomer(int id)
-    {
-    Customer.DeleteCustomer(id);
-    return RedirectToAction("Details", "Index", new { id = id });
+  
 
     [HttpGet("/customers/deleteall")]
     public ActionResult DeleteAll()
     {
       Customer.DeleteAll();
-      return View("Index");
+      return RedirectToAction("Index");
     }
   }
 }
